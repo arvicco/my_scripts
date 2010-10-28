ce509751810b32c5ef9c59f7627c5967015db880
arvicco <arvitallian@gmail.com>
2010-10-28 13:10:02 +0400

    Ansi script improved

--- lib/my_scripts/scripts/ansi.rb
+++ lib/my_scripts/scripts/ansi.rb
@@ -7,7 +7,7 @@ module MyScripts
   class Ansi < Script
     VERSION = '0.0.1'
 
-    def initialize( name, cli, argv, argf )
+    def initialize(name, cli, argv, argf)
       require 'win32console'
       @cli = cli
       @io = Win32::Console::ANSI::IO.new()
@@ -15,8 +15,14 @@ module MyScripts
     end
 
     def run
-      until cli.stdin.eof? do
-        line = cli.stdin.gets
+      Signal.trap('INT') do
+        @io.puts "ANSI Got Interrupt"
+        @io.flush
+        exit
+      end
+
+      until @cli.stdin.eof? do
+        line = @cli.stdin.gets
         @io.puts line
       end
       @io.flush
