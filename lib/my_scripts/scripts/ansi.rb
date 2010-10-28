d269ea086975054eafb848f4b9b630d049d4bad5
arvicco <arvitallian@gmail.com>
2010-10-28 12:46:53 +0400

    Ansi script added

---
+++ lib/my_scripts/scripts/ansi.rb
@@ -0,0 +1,25 @@
+module MyScripts
+  # This script should be used as a pipe to colorize ANSI output on Windows
+  # Use case is like this (provides colorized autotest output):
+  #
+  # > autotest | ansi
+  #
+  class Ansi < Script
+    VERSION = '0.0.1'
+
+    def initialize( name, cli, argv, argf )
+      require 'win32console'
+      @cli = cli
+      @io = Win32::Console::ANSI::IO.new()
+      super
+    end
+
+    def run
+      until cli.stdin.eof? do
+        line = cli.stdin.gets
+        @io.puts line
+      end
+      @io.flush
+    end
+  end
+end
